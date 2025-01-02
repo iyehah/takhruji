77360acf78dc2dc2a59e593609de9a7521c59ce8
Iyehah <117352746+iyehah@users.noreply.github.com>
2025-01-02 15:27:06 +0000

    change to HTML CSS JS

--- components/Social.jsx
+++
@@ -1,9 +0,0 @@
-export default function Social() {
-    return (
-      <div className="flex space-x-4">
-        <a href="#" className="text-gray-500 hover:text-red-500">Facebook</a>
-        <a href="#" className="text-gray-500 hover:text-red-500">Twitter</a>
-        <a href="#" className="text-gray-500 hover:text-red-500">LinkedIn</a>
-      </div>
-    );
-  }
